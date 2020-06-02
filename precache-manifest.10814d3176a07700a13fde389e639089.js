9de893bc6743f70d19b47623cab52f4d5dac41f9
USR <mozgbrasil@gmail.com>
2020-06-02 14:13:14 -0300

    Updates

---
+++ precache-manifest.10814d3176a07700a13fde389e639089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c4673ae77835765096c34e9fc016d3",
+    "url": "/react-drogaraia-test/index.html"
+  },
+  {
+    "revision": "d06aa5e00e7d329d2d7f",
+    "url": "/react-drogaraia-test/static/js/2.a4d2802a.chunk.js"
+  },
+  {
+    "revision": "468f7e79b732f9b80ec5b806f181f6cf",
+    "url": "/react-drogaraia-test/static/js/2.a4d2802a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef316ea903e891cb3ad1",
+    "url": "/react-drogaraia-test/static/js/main.b7799018.chunk.js"
+  },
+  {
+    "revision": "b4ce66607f1f8c21e0ba",
+    "url": "/react-drogaraia-test/static/js/runtime-main.6e79390f.js"
+  },
+  {
+    "revision": "27688459fc1308b9e3eecfbb406c13e3",
+    "url": "/react-drogaraia-test/static/media/dark-background.27688459.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32696226d242df1aabf5fec37410b78f.js"
+  "/react-drogaraia-test/precache-manifest.10814d3176a07700a13fde389e639089.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-drogaraia-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
